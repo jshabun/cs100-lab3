a7797f6a5d9c276ff7164cc5afb612604e7e906d
Aguyam1 <jumpman5.wm@gmail.com>
2021-04-19 02:02:05 -0700

    Updates the Op function and its test cases

--- op.hpp
+++ op.hpp
@@ -4,10 +4,20 @@
 #include "base.hpp"
 
 class Op : public Base {
-    public:
-        Op(double value) : Base() { }
-        virtual double evaluate() { return 0.0; }
-        virtual std::string stringify() { return ""; }
+  protected:
+        double val;
+        std::string str;
+
+  public:
+        Op(double value) : Base() {
+        val = value;
+        str = std::to_string(value);
+        str.erase(str.find_last_not_of('0') + 1, std::string::npos);
+        str.erase(str.find_last_not_of('.') + 1, std::string::npos);
+}
+        double evaluate() {return val; }
+        std::string stringify() { return str; }
 };
+
 
 #endif //__OP_HPP__
--- op_test.hpp
+++ op_test.hpp
@@ -10,4 +10,33 @@ TEST(OpTest, OpEvaluateNonZero) {
     EXPECT_EQ(test->evaluate(), 8);
 }
 
+TEST(OpTest, OpEvaluateZero) {
+    Op* test = new Op(0);
+    EXPECT_EQ(test->evaluate(), 0);
+}
+
+TEST(OpTest, OpEvaluateNegNonZero) {
+    Op* test = new Op(-9);
+    EXPECT_EQ(test->evaluate(), -9);
+}
+
+TEST(OpTest, OpStringifyNonZero) {
+    Op* test = new Op(8);
+   std::string eight = "8";
+    EXPECT_EQ(test->stringify(), eight);
+}
+
+TEST(OpTest, OpStringifyZero) {
+    Op* test = new Op(0);
+   std::string zero = "0";
+    EXPECT_EQ(test->stringify(), zero);
+}
+
+TEST(OpTest, OpStringifyNegNonZero) {
+    Op* test = new Op(-9);
+   std::string negnine = "-9";
+    EXPECT_EQ(test->stringify(), negnine);
+}
+
 #endif //__OP_TEST_HPP__
+
